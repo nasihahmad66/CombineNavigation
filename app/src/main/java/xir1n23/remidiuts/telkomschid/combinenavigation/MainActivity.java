e98a630cbecd977084a98d348e30ec176a324cdd
NA <nasihahmad66.na@gmail.com>
2017-06-06 16:34:56 +0700

    - functioning button

--- app/src/main/java/xir1n23/remidiuts/telkomschid/combinenavigation/MainActivity.java
+++ app/src/main/java/xir1n23/remidiuts/telkomschid/combinenavigation/MainActivity.java
@@ -1,7 +1,9 @@
 package xir1n23.remidiuts.telkomschid.combinenavigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonSwipeView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SwipeViewActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonTabView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TabViewActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonSpinnerView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SpinnerViewActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonNavDrawer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NavDrawerActivity.class));
+            }
+        });
     }
 }
